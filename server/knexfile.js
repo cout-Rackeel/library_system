22dab5155312eaf519f6e11a9d716c6a5a69d5bc
cout-Rackeel <rackeel13@gmail.com>
2025-06-24 17:07:34 -0500

    include production configurations

--- server/knexfile.js
+++ server/knexfile.js
@@ -17,5 +17,22 @@ module.exports = {
         seeds:{
             directory: './seeds'
         },
+    },
+    production: {
+        client:process.env.DB_CLIENT,
+        connection:{
+            host:process.env.DB_HOST,
+            port:process.env.DB_PORT,
+            user:process.env.DB_USER,
+            password:process.env.DB_PASSWORD,
+            database:process.env.DB,
+        },
+        migrations:{
+            directory: './migrations'
+        },
+        seeds:{
+            directory: './seeds'
+        },
     }
+
 };
