6fdbbf86e39815ea36671505cc612dc6fecd9686
cout-Rackeel <rackeel13@gmail.com>
2025-06-24 17:08:09 -0500

    updated

--- server/knexfile.js
+++ server/knexfile.js
@@ -2,22 +2,22 @@
 require("dotenv").config();
 
 module.exports = {
-    development: {
-        client:process.env.DB_CLIENT,
-        connection:{
-            host:process.env.DB_HOST,
-            port:process.env.DB_PORT,
-            user:process.env.DB_USER,
-            password:process.env.DB_PASSWORD,
-            database:process.env.DB,
-        },
-        migrations:{
-            directory: './migrations'
-        },
-        seeds:{
-            directory: './seeds'
-        },
-    },
+    // development: {
+    //     client:process.env.DB_CLIENT,
+    //     connection:{
+    //         host:process.env.DB_HOST,
+    //         port:process.env.DB_PORT,
+    //         user:process.env.DB_USER,
+    //         password:process.env.DB_PASSWORD,
+    //         database:process.env.DB,
+    //     },
+    //     migrations:{
+    //         directory: './migrations'
+    //     },
+    //     seeds:{
+    //         directory: './seeds'
+    //     },
+    // },
     production: {
         client:process.env.DB_CLIENT,
         connection:{
